e894896e291a2a8095e467b2a949ea54b0e46e33
Varunsimha Premanarasimha <varunsimha.premanarasimha@stratogent.com>
2026-04-08 05:36:51 +0530

    testing

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,16 +1,7 @@
-terraform {
-  required_providers {
-    aws = {
-        source = "hashicorp/aws"
-        version = "~>5.0"
-    }
-  }
-}
-
 provider "aws" {
     region = var.app_region
     assume_role {
-      role_arn = "arn:aws:iam::1234567890xxx:role/TerraformRole"
+      role_arn = "arn:aws:iam::590183945701:role/terraform-to-github-oidc"
       session_name = "terraform"
     }
 }
